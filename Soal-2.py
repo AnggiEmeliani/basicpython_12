8970a9d5a036185fd624417db318b49f75fde76d
AnggiEmeliani <97327009+AnggiEmeliani@users.noreply.github.com>
2022-01-29 08:53:26 +0700

    Add files via upload

---
+++ Soal-2.py
@@ -0,0 +1,8 @@
+phi = 22/7
+r = float(input("Masukan nilai jari-jari lingkaran yang diinginkan: "))
+
+luas_lingkaran = phi * (r ** 2)
+
+print(
+    f"Luas lingkaran dengan jari-jari {r} cm adalah {luas_lingkaran:.2f} cm\u00b2")
+
