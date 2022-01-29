08d40a4eb677823acc0e46acf70f657e72a2a109
AnggiEmeliani <97327009+AnggiEmeliani@users.noreply.github.com>
2022-01-29 09:46:12 +0700

    Delete Soal-2.py

--- Soal-2.py
+++
@@ -1,8 +0,0 @@
-phi = 22/7
-r = float(input("Masukan nilai jari jari lingkaran: "))
-
-luaslingkaran = phi*(r**2)
-
-print(
-    f"Luas lingkaran dengan jari jari {r} cm adalah {luaslingkaran:.2f} cm\u00b2")
-
